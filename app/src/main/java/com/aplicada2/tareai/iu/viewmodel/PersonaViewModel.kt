7dc465f279fac57ba51f63bf7a3e5c8c5101ab08
Manuel Hernandez <victor_estevez@ucne.edu.do>
2022-05-06 13:22:49 -0400

    Creación del viewmodel "Persona"

---
+++ app/src/main/java/com/aplicada2/tareai/iu/viewmodel/PersonaViewModel.kt
@@ -0,0 +1,28 @@
+package com.aplicada2.tareai.iu.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.aplicada2.tareai.data.PersonaRepository
+import com.aplicada2.tareai.data.database.PersonaDatabase
+import com.aplicada2.tareai.data.database.entities.Persona
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PersonaViewModel(application : Application): AndroidViewModel(application) {
+    private val readAllData: LiveData<List<Persona>>
+    private val repository: PersonaRepository
+
+    init{
+        val personaDao = PersonaDatabase.getDatabase(application).personaDao()
+        repository = PersonaRepository(personaDao)
+        readAllData = repository.readAllData
+    }
+
+    fun addPersona(persona: Persona){
+        viewModelScope.launch (Dispatchers.IO){
+            repository.addPersona(persona)
+        }
+    }
+}
