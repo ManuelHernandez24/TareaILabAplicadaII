6c7012615414b96aee9e69b764d2e42238266797
Manuel Hernandez <victor_estevez@ucne.edu.do>
2022-05-10 09:50:17 -0400

    Creacion de la Dao Ocupacion

---
+++ app/src/main/java/com/aplicada2/tareai/data/database/dao/OcupacionDao.kt
@@ -0,0 +1,24 @@
+package com.aplicada2.tareai.data.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.aplicada2.tareai.data.database.entities.Ocupacion
+
+@Dao
+interface OcupacionDaoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addOcupacion(ocupacion: Ocupacion)
+
+    @Update
+    suspend fun updateOcupacion(ocupacion: Ocupacion)
+
+    @Delete
+    suspend fun deleteOcupacion(ocupacion: Ocupacion)
+
+    @Query("DELETE FROM tabla_ocupacion")
+    suspend fun deleteAllOcupacion()
+
+    @Query("SELECT * FROM TABLA_OCUPACION ORDER BY OcupacionId ASC")
+    fun readAllDataOcupacion(): LiveData<List<Ocupacion>>
+
+}
